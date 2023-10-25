d17c75e49114404e0f89ee22537a535b6c3df7b3
ANSH KAUSHIK <anshkaushik9454@gmail.com>
2023-10-25 23:49:27 +0530

    cors policies updated

--- index.js
+++ index.js
@@ -38,6 +38,21 @@ app.use(mongoSanitize());
 //data sanitization against XSS
 app.use(xss());
 
+//implemneting cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Origin, X-Requested-With'
+  );
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 //prevent parameter pollution
 app.use(
   hpp({
